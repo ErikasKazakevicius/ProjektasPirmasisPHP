fa45696ca6d67cedc210c4c9470ab161aa2ebc04
ErikasKazakevicius <kazakevicius.erikas@yahoo.com>
2019-10-29 16:31:16 +0200

    Uzduotis

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Ash valdau HTML!</title>
+    <title><?php print 'Aš, '. date('l') . ' ir PHP' ?> !</title>
   </head>
   <body>
-    <h1>Erikas - HTML asas</h1>
+      <h1>Erikas - HTML <?php print 'ir PHP' ?> asas jau nuo <?php print date('Y').' metų' ?> </h1>
     <p>Moku kurti statinį turinį</p>
+    <p>Viskas prasidėjo:  <?php print date('m').' mėnesio '. date('d').' dieną';?></p>
   </body>
 </html>
