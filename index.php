4d6941d0461614d435ddcf42b6b0f9ff3732a31a
ErikasKazakevicius <kazakevicius.erikas@yahoo.com>
2019-10-29 15:06:33 +0200

    Uzduotis

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-print 'Veikia!';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ash valdau HTML!</title>
+  </head>
+  <body>
+    <h1>Erikas - HTML asas</h1>
+    <p>Moku kurti statinį turinį</p>
+  </body>
+</html>
